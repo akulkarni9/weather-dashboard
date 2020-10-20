c5c3bbda7b96497acbe1a400a429affc3483ee72
kulkaa <ajaykumar.kulkarni@dell.com>
2020-10-20 17:06:09 +0530

    Modified test files

--- src/components/tests/componentLoader.test.js
+++ src/components/tests/componentLoader.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
+import NavBar from '../navbar';
 import Dashboard from '../dashboard';
 
-test('renders Navbar', () => {
+test('renders dashboard', () => {
+  const getByTextOne  = render(<NavBar />);
+  const linkedElement = getByTextOne(/Weather/);
+  expect(linkedElement).toBeInTheDocument();
+});
+
+test('renders Dashboard', () => {
   const { getByText } = render(<Dashboard />);
   const linkElement = getByText(/Loading/);
   expect(linkElement).toBeInTheDocument();
